242f335f9e6601323475dfb34f4713be691d8733
NathanJolivetWeb <nathan.jolivet@gmail.com>
2023-03-29 01:28:45 +0200

    crud with react rails

---
+++ app/javascript/components/Editor.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import EventList from './EventList';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Event from './Event';
+import EventForm from './EventForm';
+
+const Editor = () => {
+    const [events, setEvents] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await window.fetch('/api/events');
+                if (!response.ok) throw Error(response.statusText);
+                const data = await response.json();
+                setEvents(data);
+            } catch (error) {
+                setIsError(true);
+                console.error(error);
+            }
+
+            setIsLoading(false);
+        };
+
+        fetchData();
+    }, []);
+
+    const addEvent = async (newEvent) => {
+        try {
+            const response = await window.fetch('/api/events', {
+                method: 'POST',
+                body: JSON.stringify(newEvent),
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!response.ok) throw Error(response.statusText);
+
+            const savedEvent = await response.json();
+            const newEvents = [...events, savedEvent];
+            setEvents(newEvents);
+            window.alert('Event Added!');
+            navigate(`/events/${savedEvent.id}`);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const deleteEvent = async (eventId) => {
+        const sure = window.confirm('Are you sure?');
+
+        if (sure) {
+            try {
+                const response = await window.fetch(`/api/events/${eventId}`, {
+                    method: 'DELETE',
+                });
+
+                if (!response.ok) throw Error(response.statusText);
+
+                window.alert('Event Deleted!');
+                navigate('/events');
+                setEvents(events.filter(event => event.id !== eventId));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+    const updateEvent = async (updatedEvent) => {
+        try {
+            const response = await window.fetch(
+                `/api/events/${updatedEvent.id}`,
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(updatedEvent),
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            if (!response.ok) throw Error(response.statusText);
+
+            const newEvents = events;
+            const idx = newEvents.findIndex((event) => event.id === updatedEvent.id);
+            newEvents[idx] = updatedEvent;
+            setEvents(newEvents);
+
+            success('Event Updated!');
+            navigate(`/events/${updatedEvent.id}`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <Header />
+            <div className="grid">
+                {isError && <p>Something went wrong. Check the console.</p>}
+                {isLoading ? (
+                    <p className='loading'>Loading...</p>
+                ) : (
+                    <>
+                        <EventList events={events} />
+                        <Routes>
+                            <Route
+                                path=":id"
+                                element={<Event events={events} onDelete={deleteEvent} />}
+                            />
+                            <Route
+                                path=":id/edit"
+                                element={<EventForm events={events} onSave={updateEvent} />}
+                            />
+                            <Route path="new" element={<EventForm onSave={addEvent} />} />
+                        </Routes>
+
+                    </>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Editor;
